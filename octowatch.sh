ee4c123fbb8840b52df1f51e15462834b67eeb86
Vincent Verdot <vincent.verdot@gmail.com>
2018-07-20 14:08:36 +0200

    Fixed error message to be more specific.

--- octowatch.sh
+++ octowatch.sh
@@ -16,13 +16,15 @@
 
 VERSION="0.3"
 GITHUB_API="https://api.github.com/repos"
+GITHUB_TOKEN=
+
 OW_CONFIG_DIR="$HOME/.octowatch.d"
 # API type [undefined|github|gitlab]
 API_TYPE="undefined"
 
 # Default delay: 1 day (86400 seconds)
 OW_CACHE_DELAY=86400
-OW_TIMEOUT=5
+OW_TIMEOUT=10
 OW_CACHE_FILE="$OW_CONFIG_DIR/cache.json"
 OW_WATCH_FILE="$OW_CONFIG_DIR/watch.lst"
 OW_CONF_FILE="$OW_CONFIG_DIR/config"
@@ -35,6 +37,10 @@ fi
 
 [[ $https_proxy ]] && export https_proxy
 
+GITHUB_AUTH=''
+if [ -n "$GITHUB_TOKEN" ]; then
+    GITHUB_AUTH="-H \"Authorization: token $GITHUB_TOKEN\""
+fi
 CURL_CMD="curl -sSL -m $OW_TIMEOUT "
 
 ## Utility functions
@@ -99,6 +105,26 @@ msg() {
 
 ## Functions
 
+# $1 repo name
+# $2 branch
+# $3 git host
+getCommit() {
+    case "$API_TYPE" in
+        github)
+            API_URL="$GITHUB_API/$1/branches/$2" 
+            COMMIT=$(eval "$CURL_CMD $GITHUB_AUTH $API_URL" | jq -r '.commit.sha')
+            ;;
+        gitlab)
+            API_URL="https://$3/api/v4/projects/${1//\//\%2F}/repository/branches/$2"
+            COMMIT=$($CURL_CMD ${API_URL} | jq -r '.commit.id')
+            ;;
+        *) return 1            
+            ;;
+    esac
+
+    return 0
+}
+
 checkWithCache() {
     cd "$2"
     current=$(git rev-parse HEAD)
@@ -109,38 +135,39 @@ checkWithCache() {
 
     # Check if working dir is not ahead
     # git rev-list @ |grep -q "^befb103a12dbc9f2a566c41cdb86b517ec4094bd$"
-    $(git rev-list @ |grep -q \"^$cached\$\")
+    $(git rev-list @ |grep -q "^$cached$")
+    if [ $? -eq 0 ]; then
+        return 2
+    fi
 
-    return "$?"
+    return 1
 }
 
 refreshCache() {
+    F_NEW_ENTRY=${4:-0}
     
-    GIT_API="$GITHUB_API/$1/branches/$2"
-    F_NEW_ENTRY=${3:-0}
+    # COMMIT is set in getCommit 
+    COMMIT=
+    getCommit "$1" "$2" "$3"
    
-    # TODO: GITLAB COMPAT
-    # wget -O- https://git.verdot.fr/api/v4/projects/vverdot%2Ftools/repository/branches/master | jq -r '.commit.id'
-    COMMIT=$($CURL_CMD ${GIT_API} | jq -r '.commit.sha')
-
     # Check GIT API call result
     if [ "$?" -ne 0 ] || [ -z "$COMMIT" ] ; then
-        msg "Error accessing Github API. [$GIT_API]."
+        msg "Error accessing Git API. [$3]."
         exit 1
     fi
 
     if [ "$COMMIT" == "null" ] ; then
-        msg "No commit found! Aborting."
+        msg "Cannot retrieve version info from git server."
         exit 1
     fi
 
     tmpfile=$(mktemp /tmp/octowatch.XXX)
 
     if [ "$F_NEW_ENTRY" == 0 ] ; then
-        jq "(.[] | select(.repository==\"$1\" and .branch==\"$2\") | .verified) |= \"$(date +%s)\" | \
-        (.[] | select(.repository==\"$1\" and .branch==\"$2\") | .commit) |= \"$COMMIT\"" "$OW_CACHE_FILE" > "$tmpfile"
+        jq "(.[] | select(.repository==\"$1\" and .branch==\"$2\" and .host==\"$3\") | .verified) |= \"$(date +%s)\" | \
+        (.[] | select(.repository==\"$1\" and .branch==\"$2\" and .host==\"$3\") | .commit) |= \"$COMMIT\"" "$OW_CACHE_FILE" > "$tmpfile"
     else
-        jsonStr="{\"repository\": \"${1}\",\"branch\": \"${2}\",\"verified\": \"$(date +%s)\", \"commit\": \"${COMMIT}\"}"
+        jsonStr="{\"repository\": \"${1}\",\"branch\": \"${2}\",\"host\":\"${3}\",\"verified\": \"$(date +%s)\", \"commit\": \"${COMMIT}\"}"
         jq ". |= (.+ [$jsonStr])" "$OW_CACHE_FILE" > "$tmpfile"
     fi
 
@@ -154,7 +181,7 @@ refreshCache() {
 }
 
 getCache() {
-    CACHED_DATA=$(jq "[.[] | select(.repository==\"$1\" and .branch==\"$2\")] | .[0]" "$OW_CACHE_FILE")
+    CACHED_DATA=$(jq "[.[] | select(.repository==\"$1\" and .branch==\"$2\" and .host==\"$3\")] | .[0]" "$OW_CACHE_FILE")
 
     # Check parsing problems
     if [ "$CACHED_DATA" == "" ] ; then
@@ -163,18 +190,16 @@ getCache() {
     fi
 }
 
-# $1 repository remote url
+# $1 repository host name
 #   + provided by:  git config --get remote.origin.url
 detectAPI() {
     
     [ $# -ne 1 ] && return 1;
-
-    GIT_HOST=${1#*//} && GIT_HOST=${GIT_HOST%%/*}
     
-    if [[ "$GIT_HOST" == *"github.com"* ]] ; then
+    if [[ "$1" == *"github.com"* ]] ; then
         API_TYPE="github"
     else
-        CURL_REPLY=$(curl -sSL -m 5 https://$GIT_HOST/api/v4/version | jq -r '.message' 2>/dev/null)
+        CURL_REPLY=$(curl -sSL -m 5 https://$1/api/v4/version | jq -r '.message' 2>/dev/null)
         if [ $? -eq 0 ] && [ "$CURL_REPLY" == "401 Unauthorized" ]; then
             API_TYPE="gitlab"
         fi
@@ -201,51 +226,54 @@ printRepoStatus() {
 
     # Get git remote.origin.url
     GIT_REMOTE=$(git config --get remote.origin.url)
+    GIT_HOST=${GIT_REMOTE#*//} && GIT_HOST=${GIT_HOST%%/*} && GIT_HOST=${GIT_HOST#*@}
     GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
     if [ "$?" -ne 0 ] ; then
         msg "Invalid GIT Repository [$1]"
         return 1
     fi
 
-    detectAPI $GIT_REMOTE
-    msg "Repository detected [$API_TYPE]"
+    detectAPI $GIT_HOST
 
-    # TODO: GIT_REPO Name extraction based on APÏ type
+    # DEBUG
+    #msg "Repository detected [$API_TYPE]"
 
     # Get full repo name
-    GIT_REPO="${GIT_REMOTE#*github.com/}"
+    GIT_REPO="${GIT_REMOTE#*$GIT_HOST/}"
     GIT_REPO="${GIT_REPO%'.git'}"
 
     CACHED_DATA="null"
 
     if [ -f "$OW_CACHE_FILE" ] ; then
-        getCache "$GIT_REPO" "$GIT_BRANCH"
+        getCache "$GIT_REPO" "$GIT_BRANCH" "$GIT_HOST"
     else
         echo '[]' > "$OW_CACHE_FILE"
     fi
 
     if [ "$CACHED_DATA" == "null" ] ; then
-        refreshCache "$GIT_REPO" "$GIT_BRANCH" 1
-        getCache "$GIT_REPO" "$GIT_BRANCH"
+        refreshCache "$GIT_REPO" "$GIT_BRANCH" "$GIT_HOST" 1
+        getCache "$GIT_REPO" "$GIT_BRANCH" "$GIT_HOST"
     fi
 
     last_verification=$(jq -r '.verified' <<< "$CACHED_DATA")
     is_expired=$(( ($(date +%s) - $last_verification) > $OW_CACHE_DELAY ))
 
     if [ $is_expired -eq 1 ] ; then
-        refreshCache "$GIT_REPO" "$GIT_BRANCH"
-        getCache "$GIT_REPO" "$GIT_BRANCH"
+        refreshCache "$GIT_REPO" "$GIT_BRANCH" "$GIT_HOST"
+        getCache "$GIT_REPO" "$GIT_BRANCH" "$GIT_HOST"
     fi
 
     checkWithCache "$CACHED_DATA" "$repoDir"
+    CHECK=$?
 
-    if [ $? -eq 0 ] ; then
+    if [ $CHECK -eq 0 ]; then
+        [[ $F_UPDATES ]] && return 0;
         returnStr="[$bold${green}uptodate$normal]"
-        if [[ $F_UPDATES ]] ; then
-            return 0
-        fi
-    else
+    elif [ $CHECK -eq 1 ]; then
         returnStr="[$bold${red}outdated$normal]"
+    elif [ $CHECK -eq 2 ]; then
+        [[ $F_UPDATES ]] && return 0;
+        returnStr="[$bold${green}-> ahead$normal]"
     fi
 
     echo " $returnStr $repoDir ($GIT_REPO)"
